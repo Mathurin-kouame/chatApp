d31baa383859839a3a3b2fce26ef413eee9bda3c
Kouame Mathurin Yao <kouame16.yao@uvci.edu.ci>
2024-09-19 00:17:26 +0000

    fin de la page chat

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -6,6 +6,8 @@ import imgHelp from '../../assets/icons8-info-128.png'
 import pointVert from '../../assets/icons8-point-final-50.png'
 import galery from '../../assets/icons8-galery-64.png'
 import send from '../../assets/icons8-envoyer-48.png'
+import sendImage from '../../assets/2044263_magnifying glass_search_icon.png'
+import photo from '../../assets/image-s.jpg'
 
 const ChatBox = () => {
   return (
@@ -15,6 +17,38 @@ const ChatBox = () => {
         <p>Rafael ange <img className='dot' src={pointVert} alt="" /></p>
         <img src={imgHelp} alt="" className='help' />
       </div>
+
+      <div className="chat-msg">
+        <div className="s-msg">
+         <p className='msg'>Bonsoir comment vous aller</p>
+          <div>
+            <img  src={profilImage} alt="" />
+            <p>14:18</p>
+          </div>
+        </div>
+        <div className="s-msg">
+          <img className='msg-img' src={photo} alt="" />
+          <div>
+            <img src={profilImage} alt="" />
+            <p>14:18</p>
+          </div>
+        </div>
+        <div className="r-msg">
+          <p className="msg">message ecrire par john</p>
+          <div>
+            <img src={profilImage} alt="" />
+            <p>14:18</p>
+          </div>
+        </div>        
+        <div className="r-msg">
+          <p className="msg">message ecrire par john</p>
+          <div>
+            <img src={profilImage} alt="" />
+            <p>14:18</p>
+          </div>
+        </div>        
+    </div>
+
       <div className="chat-input">
         <input type="text" placeholder='envoyé un message' />
         <input type="file" id='image' accept='image/png, image/jpg' hidden />
--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -1,10 +1,31 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import './RightSidebar.css'
+import prImage from '../../assets/profil-image.jpg'
+import pointvert from '../../assets/icons8-point-final-50.png'
+import photo from '../../assets/image-s.jpg'
+
 const RightSideBar = () => {
   return (
-    <div>
-      
+    <div className='rs'>
+      <div className="profile">
+        <img src={prImage} alt="" />
+        <h3>Rafael Ange <img  className="dot" src={pointvert} alt="" /> </h3>
+         <p>j ai téléchargé chatApp pour causer avec toi</p>
+      </div>
+      <hr />
+      <div className="rs-media">
+        <p>Media</p>
+        <div>
+          <img src= {photo } alt="" />
+          <img src= {photo} alt="" />
+          <img src= {photo } alt="" />
+          <img src= {photo } alt="" />
+          <img src= {photo } alt="" />
+          <img src= {photo } alt="" />
+        </div>
+      </div>
+      <button>se déconnecter</button>
     </div>
   )
 }
